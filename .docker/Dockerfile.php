78aafba691f9ce6684763a86635728fe011830f1
ahmad <ahmad1510520140@gmail.com>
2024-08-14 13:44:34 +0800

    Update Dockerfile.php

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -1,4 +1,13 @@
 FROM php:7.4-fpm-alpine
-RUN apk add --no-cache freetype libpng libjpeg-turbo freetype-dev libpng-dev libjpeg-turbo-dev zlib-dev libzip-dev tidyhtml-dev curl-dev && \
-    docker-php-ext-install pdo pdo_mysql mysqli zip bcmath gd json tidy curl fileinfo exif
+
+# Install necessary packages and PHP extensions
+RUN apk add --no-cache freetype libpng libjpeg-turbo freetype-dev libpng-dev libjpeg-turbo-dev zlib-dev libzip-dev tidyhtml-dev curl-dev \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
+    && docker-php-ext-install pdo pdo_mysql mysqli zip bcmath gd json tidy curl fileinfo exif
+
+# Install Imagick dependencies and Imagick extension from source
+RUN apk add --no-cache autoconf g++ imagemagick imagemagick-dev make \
+    && pecl install imagick \
+    && docker-php-ext-enable imagick
+
 EXPOSE 9000
